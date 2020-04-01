c5939c583817525b8cd082007103c766234b2017
Tommy Boone <tommyboone23@Tommys-MacBook-Air.local>
2020-03-31 19:08:52 -0500

    api-routes built

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var PORT = process.env.PORT || 8080;
 var db = require("./models");
 // Creating express app and configuring middleware needed for authentication
 var app = express();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
@@ -23,6 +24,7 @@ app.set("view engine", "handlebars");
 // Requiring our routes
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
+
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
