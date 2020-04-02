fe2df7055d5bab76938c2f70a3edb84377f38d5f
Lauren Hallau <lhallau@gmail.com>
2020-04-02 13:30:33 -0500

    login page fixed

--- models/index.js
+++ models/index.js
@@ -3,12 +3,13 @@
 var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
-var basename = path.basename(__filename);
+var basename = path.basename(module.filename);
 var env = process.env.NODE_ENV || "development";
 var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
 
 var sequelize;
+
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
--- public/js/login.js
+++ public/js/login.js
@@ -30,7 +30,7 @@ $(document).ready(function() {
       password: password
     })
       .then(function() {
-        window.location.replace("/userhomepage");
+        window.location.replace("/homepage");
         // If there's an error, log the error
       })
       .catch(function(err) {
