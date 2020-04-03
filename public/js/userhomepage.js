9dfaaa1a42880ddcd4e49196b60eb3dc6298d0b2
Kelly Bazella <kelly.bazella@gmail.com>
2020-04-02 19:05:04 -0500

    js file for userhomepage

---
+++ public/js/userhomepage.js
@@ -0,0 +1,24 @@
+$().ready(function() {
+  function renderUserQuizzes() {
+    $.ajax("/api/quizzes",{
+      method: "GET"
+    }).then(function(res, req) {
+      var username = req.body.name;
+      var quiz = req.body.title;
+      $("#user-name").text(username);
+      $(".user-quizzes").text(quiz);
+    });
+  }
+  function renderUserScores() {
+    $.ajax("/api/quizzes",{
+      method: "GET"
+    }).then(function(res, req) {
+      var pastQuiz = req.body.title;
+      var pastScore = req.body.score;
+      $(".past-quiz").text(pastQuiz);
+      $(".past-quiz-score").text(pastScore);
+    });
+  }
+  renderUserQuizzes();
+  renderUserScores();
+});
