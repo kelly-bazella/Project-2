04eafe48deb1341a9a87bcda895166c075843e2d
Kelly Bazella <kelly.bazella@gmail.com>
2020-04-03 22:53:25 -0500

    connecting front to back

--- public/js/createquiz.js
+++ public/js/createquiz.js
@@ -1,1 +1,12 @@
 $(".dropdown-trigger").dropdown();
+
+$(".create-quiz").on("click", function(event) {
+  event.preventDefault();
+  var newQuiz = {
+    title: $("#quiz_title")
+      .val()
+      .trim(),
+    category: $("#category-dropdown a").text()
+  };
+  console.log(newQuiz);
+});
