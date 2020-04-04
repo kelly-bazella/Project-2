69196a61bde04e62042fd24cdada0097d4a67062
Tommy Boone <tommyboone23@Tommys-MacBook-Air.local>
2020-04-03 19:35:29 -0500

    quizzes rendering on page

--- public/js/currentquiz.js
+++ public/js/currentquiz.js
@@ -1,5 +1,24 @@
-// $(document).ready(function() {
-//   $.get("/api/currentquiz").then(function(data) {
-//     $("#quiz-title").text(data.title);
-//   });
-// });
+var db = require("../../models");
+var score = 0;
+var currentQuestion = 0;
+
+$(document).ready(function() {
+  $.get("/api/currentquiz").then(function(data) {
+    $("#quiz-title").text(data.title);
+  });
+
+  function checkAnswer() {
+    var question = db.questions[currentQuestion];
+    var out = $(".validate").val();
+    console.log(out);
+    console.log(question.answer);
+    if (out === question.answer) {
+      score++;
+      console.log(score);
+    }
+  }
+  $(".submit-quiz").on("click", function(event) {
+    event.preventDefault();
+    checkAnswer();
+  });
+});
--- public/js/takequiz.js
+++ public/js/takequiz.js
@@ -1,10 +1,17 @@
-$().ready(function() {
+$(document).ready(function() {
   $(".take-random-quiz").on("click", function() {
-
     //take user to random quiz to complete
   });
 
   $(".take-quiz").on("click", function() {
-    //take the index number, find it in database and present user with quiz
+    $.get("api/quizzes", function(data) {
+      var responseArray = [];
+      for (var i = 0; i < data.length; i++) {
+        responseArray.push(data);
+        console.log(responseArray[i]);
+      }
+    });
   });
+
+  //take the index number, find it in database and present user with quiz
 });
