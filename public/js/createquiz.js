a348b7463080f44d20513decc32a59d635942386
Tommy Boone <tommyboone23@Tommys-MacBook-Air.local>
2020-04-04 13:42:37 -0500

    quizzes posting to db

--- public/js/createquiz.js
+++ public/js/createquiz.js
@@ -1,12 +1,35 @@
 $(".dropdown-trigger").dropdown();
-
-$(".create-quiz").on("click", function(event) {
-  event.preventDefault();
-  var newQuiz = {
-    title: $("#quiz_title")
-      .val()
-      .trim(),
-    category: $("#category-dropdown a").text()
-  };
-  console.log(newQuiz);
+$(document).ready(function() {
+  $("select").formSelect();
+  $(".create-quiz").on("click", function(event) {
+    event.preventDefault();
+    console.log("hey");
+    var quizArray = [
+      {
+        question: $("#first_question").val(),
+        answer: $("#first_answer").val()
+      },
+      {
+        question: $("#second_question").val(),
+        answer: $("#second_answer").val()
+      },
+      {
+        question: $("#third_question").val(),
+        answer: $("#third_answer").val()
+      }
+    ];
+    console.log(quizArray);
+    var newQuiz = {
+      title: $("#quiz_title")
+        .val()
+        .trim(),
+      category: $("#category-dropdown").val(),
+      numberOfQuestions: 3,
+      questions: quizArray
+    };
+    $.post("/api/createquiz", newQuiz, function(cb) {
+      console.log(newQuiz);
+      console.log(cb);
+    });
+  });
 });
