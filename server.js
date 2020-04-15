e6ad4950e81b0745bccf1e48f73d4460d94546d0
Lauren Hallau <lhallau@gmail.com>
2020-04-14 19:40:09 -0500

    adding the middleware compression data

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-
 // Requiring necessary npm packages
 var express = require("express");
 var session = require("express-session");
 var handlebars = require("express-handlebars");
+var compression = require("compression");
+
 // Requiring passport as we've configured it
 var passport = require("./config/passport");
 // Setting up port and requiring models for syncing
@@ -11,23 +12,40 @@ var db = require("./models");
 // Creating express app and configuring middleware needed for authentication
 var app = express();
 
+// eslint-disable-next-line no-use-before-define
+app.use(compression({ filter: shouldCompress }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 // We need to use sessions to keep track of our user's login status
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 // setting up handlebars
-app.engine("handlebars", handlebars ({defaultLayout:"main"}));
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 // Requiring our routes
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
-    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+db.sequelize.sync().then(function () {
+  app.listen(PORT, function () {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
   });
 });
+
+function shouldCompress(req, res) {
+  if (req.headers["x-no-compression"]) {
+    // don't compress responses with this request header
+    return false;
+  }
+  // fallback to standard filter function
+  return compression.filter(req, res);
+}
