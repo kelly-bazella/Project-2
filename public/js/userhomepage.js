7945790aeabb98dc5fb8a506b620149510e62d6c
Lauren Hallau <lhallau@gmail.com>
2020-04-17 17:27:38 -0500

    getting user name to show up on homepage

--- public/js/userhomepage.js
+++ public/js/userhomepage.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get("/api/user_data").then(function(data) {
-    $(".member-name").text(data.email);
+    $("#user-name").text(data.email);
   });
   function renderUserQuizzes() {
     $.ajax("/api/quizzes", {
